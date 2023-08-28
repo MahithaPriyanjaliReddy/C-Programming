aff5f1972fe16278a2ae258a81bac32f5874f9da
mahithapriyanjalireddy <priyanjali6884@gmail.com>
2023-08-28 23:53:08 +0530

    avg of student's marks

---
+++ conditional_statement/marks.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int x,y,z,sum,avg;
+	printf("input x value: ");
+	scanf("%d", &x);
+	printf("input y value: ");
+	scanf("%d", &y);
+	printf("input z value: ");
+	scanf("%d", &z);
+	sum=x+y+z;
+	avg=sum/3;
+	if(avg>85){
+	printf("high achiever");
+}
+	else if(70>avg<85){
+	printf("pass");
+}
+	else if(avg<70){
+	printf("needs an improvement");
+}
+return 0;
+}
